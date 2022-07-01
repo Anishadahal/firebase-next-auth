b725f76d2a42c3601f0ccabb3afa846ace91653c
Anisha Dahal <anisha.dahal@webo.digital>
2022-07-01 14:48:08 +0545

    add route

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -39,7 +39,11 @@ const Header = ({ children }: { children: React.ReactNode }) => {
 		<>
 			<header className="flex flex-wrap container mx-auto max-w-full items-center p-6 justify-between bg-white shadow-md sticky top-0 z-50">
 				<div className="flex items-center text-blue-900 hover:text-blue-800 cursor-pointer transition duration-150 ">
-					<span className="font-semibold text-lg font-sans">Authentication Demo</span>
+					<Link href={HOME}>
+						<span className="font-semibold text-lg font-sans">
+							Firebase Authentication Demo
+						</span>
+					</Link>{" "}
 				</div>
 
 				<nav className={`md:flex md:items-center font-title w-full md:w-auto`}>
