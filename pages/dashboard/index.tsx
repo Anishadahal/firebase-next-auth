7efb2bb6381e5f4e6cc11979c06dd2958d68c31a
Anisha Dahal <anisha.dahal@webo.digital>
2022-07-01 14:45:38 +0545

    add dashboard content

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -4,7 +4,11 @@ import ProtectedRoute from "../../components/protected-route";
 const DashboardPage = () => {
 	return (
 		<ProtectedRoute>
-			<div>You are logged in!</div>
+			<div className="flex py-2 container mx-auto">
+				<div className="text-gray-600 px-12 py-24 mt-24 overflow-y-hidden mx-auto">
+					<h2 className="text-2xl font-semibold">You are logged in!</h2>
+				</div>
+			</div>
 		</ProtectedRoute>
 	);
 };
